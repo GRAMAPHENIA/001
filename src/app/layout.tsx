1860527e10dafcf513e23d2653eb7e37601ee153
gramaphenia <dicoratojuanpablo@gmail.com>
2023-12-20 06:29:07 -0300

    mods

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CONCEPTO",
-  description: "Generada por RedFang & BirdieOnBoard",
+  title: "CONCEPTO HEXAGONO",
+  description: "Sitio dedicado a la creación de páginas web, generado por RedFang & BirdieOnBoard.",
 };
 
 export default function RootLayout({
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="es">
       <body className={`${inter.className} antialiased`}>
         <header className="navegacion justify-between items-center">
-          <div className="h-11 w-11 rounded-full border-2 border-[#213f4e]">
+          <div className="ml-2 h-11 w-11 rounded-full border-2 border-[#213f4e]">
             <Image
               className="p-2 justify-center align-middle"
               src="/iconos/logo.svg"
@@ -35,13 +35,15 @@ export default function RootLayout({
             <Navegacion />
           </div>
           <div className="md:hidden">
-            <h1 className="text-white"><Image
-              className="p-2 justify-center align-middle"
-              src="/iconos/hexagono.svg"
-              width={50}
-              height={50}
-              alt="Logo de Hexagono"
-            ></Image></h1>
+            <h1 className="text-white">
+              <Image
+                className="p-2 justify-center align-middle"
+                src="/iconos/hexagono.svg"
+                width={50}
+                height={50}
+                alt="Logo de Hexagono"
+              ></Image>
+            </h1>
           </div>
         </header>
         {children}
