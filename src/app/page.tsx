74a69674193c7eeefa4a8be840a669adf4d79a9e
gramaphenia <dicoratojuanpablo@gmail.com>
2023-12-26 09:22:55 -0300

    mods

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,30 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col mx-2 md:mx-20 ">
-      <section className="section-a rounded-lg">
-        <div className="flex justify-between items-center h-20 shrink-0 rounded-lg bg-[var(--dark)] md:h-[35rem] font-bold text-[var(--font-light)] mt-20">
-          <div className="bg-[var(--card-dark)] w-full h-[35rem] rounded-xl text-right overflow-hidden">
-            <div className="flex justify-start bg-amber-100/20 w-[22rem] h-[22rem] rounded-br-full"></div>
-          </div>
-
-          <article className="border border-[var(--button-dark)] w-[400px] h-[35rem] rounded-l-lg text-center flex flex-col justify-center align-middle ml-2">
-            <h1 className="text-[var(--button-light)] text-xl tracking-tight font-mono leading-7">
-              DESARROLLO
-            </h1>
-            <p className="text-[var(--button-light)] text-[67px] tracking-tight font-mono leading-8">
-              WEB
-            </p>
-          </article>
+    <main className="flex flex-col md:mx-20 ">
+      <section className="text-[var(--font-light)] grid grid-cols-1 md:grid-cols-2 mt-20 gap-4 mx-4">
+        <div className="w-full h-96 bg-[var(--hover)] rounded-xl p-2">
+          <h1 className="text-4xl uppercase">desarrollo web</h1>
+          <p>
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+            earum officiis, hic eaque et natus cupiditate tempora beatae
+            laboriosam, consequuntur quae. Quaerat facilis blanditiis impedit
+            pariatur dolor, nostrum veritatis. Temporibus.
+          </p>
+          <br />
+          <small>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Perspiciatis explicabo velit incidunt deleniti cum ut atque
+          </small>
+          <br />
+          <br />
+          <button>Boton</button>
         </div>
-      </section>
 
-      <section className="grid grid-cols-4 text-white">
-        <article className="col-start-1 col-end-3 bg-slate-300">articulo uno</article>
-        <article className="col-start-3 col-end-4 bg-slate-700">articulo uno</article>
+        <div className="w-full h-96 bg-[var(--hover)] rounded-xl p-2">
+          imagen
+        </div>
       </section>
     </main>
   );
