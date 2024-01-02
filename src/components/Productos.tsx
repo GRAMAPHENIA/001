47283e8c29ed552afca9413a8faf2388185f7523
gramaphenia <dicoratojuanpablo@gmail.com>
2024-01-02 12:15:05 -0300

    fixs-navs-complete

--- src/components/Productos.tsx
+++ src/components/Productos.tsx
@@ -12,9 +12,9 @@ const Productos = () => {
       <div className="flex flex-col text-[var(--font-light)] profile">
         <ul className="flex flex-col gap-4 ">
           <Link href="/">
-            <li className="flex items-center mx-0 hover:text-[var(--gradient-via)]">
+            <li className="flex group items-center mx-0 hover:text-[var(--gradient-via)]">
               <Image
-                className="mr-4 border border-slate-700 p-1 rounded-full"
+                className="mr-4 group-hover:bg-[var(--border-card-dark)] group-hover:border-[var(--gradient-via)] border border-slate-700 p-1 rounded-full"
                 src="/iconos/facebook.svg"
                 width={38}
                 height={38}
@@ -24,9 +24,9 @@ const Productos = () => {
             </li>
           </Link>
           <Link href="/">
-            <li className="flex items-center mx-0 hover:text-[var(--gradient-via)]">
+            <li className="flex group items-center mx-0 hover:text-[var(--gradient-via)]">
               <Image
-                className="mr-4 border border-slate-700 p-1 rounded-full"
+                className="mr-4 group-hover:bg-[var(--border-card-dark)] group-hover:border-[var(--gradient-via)] border border-slate-700 p-1 rounded-full"
                 src="/iconos/instagram.svg"
                 width={38}
                 height={38}
@@ -36,9 +36,9 @@ const Productos = () => {
             </li>
           </Link>
           <Link href="/">
-            <li className="flex items-center mx-0 hover:text-[var(--gradient-via)]">
+            <li className="flex group items-center mx-0 hover:text-[var(--gradient-via)]">
               <Image
-                className="mr-4 border border-slate-700 p-1 rounded-full"
+                className="mr-4 group-hover:bg-[var(--border-card-dark)] group-hover:border-[var(--gradient-via)] border border-slate-700 p-1 rounded-full"
                 src="/iconos/email.svg"
                 width={36}
                 height={36}
@@ -48,15 +48,15 @@ const Productos = () => {
             </li>
           </Link>
           <Link href="/">
-            <li className="flex items-center mx-0 hover:text-[var(--gradient-via)]">
+            <li className="flex group items-center mx-0 hover:text-[var(--gradient-via)]">
               <Image
-                className="mr-4 border border-slate-700 p-1 rounded-full"
-                src="/iconos/email.svg"
+                className="mr-4 group-hover:bg-[var(--border-card-dark)] group-hover:border-[var(--gradient-via)] border border-slate-700 p-1 rounded-full"
+                src="/iconos/whatsapp.svg"
                 width={36}
                 height={36}
                 alt="Picture of the author"
               ></Image>
-              Email
+              WhatsApp
             </li>
           </Link>
         </ul>
