0860fd3a848071599939a92f95431587c79d768a
gramaphenia <dicoratojuanpablo@gmail.com>
2024-01-02 09:57:45 -0300

    globals

--- src/components/Productos.tsx
+++ src/components/Productos.tsx
@@ -10,38 +10,50 @@ const Productos = () => {
   return (
     <>
       <div className="flex flex-col text-[var(--font-light)] profile">
-        <ul className="flex flex-col gap-4">
+        <ul className="flex flex-col gap-4 ">
           <Link href="/">
-            <li className="flex items-center mx-0">
+            <li className="flex items-center mx-0 hover:text-[var(--gradient-via)]">
               <Image
-                className="mr-4"
+                className="mr-4 border border-slate-700 p-1 rounded-full"
                 src="/iconos/facebook.svg"
-                width={30}
-                height={30}
+                width={38}
+                height={38}
                 alt="Picture of the author"
               ></Image>
               Facebook
             </li>
           </Link>
           <Link href="/">
-            <li className="flex items-center mx-0">
+            <li className="flex items-center mx-0 hover:text-[var(--gradient-via)]">
               <Image
-                className="mr-4"
+                className="mr-4 border border-slate-700 p-1 rounded-full"
                 src="/iconos/instagram.svg"
-                width={30}
-                height={30}
+                width={38}
+                height={38}
                 alt="Picture of the author"
               ></Image>
               Instagram
             </li>
           </Link>
           <Link href="/">
-            <li className="flex items-center mx-0">
+            <li className="flex items-center mx-0 hover:text-[var(--gradient-via)]">
               <Image
-                className="mr-4"
+                className="mr-4 border border-slate-700 p-1 rounded-full"
                 src="/iconos/email.svg"
-                width={28}
-                height={28}
+                width={36}
+                height={36}
+                alt="Picture of the author"
+              ></Image>
+              Email
+            </li>
+          </Link>
+          <Link href="/">
+            <li className="flex items-center mx-0 hover:text-[var(--gradient-via)]">
+              <Image
+                className="mr-4 border border-slate-700 p-1 rounded-full"
+                src="/iconos/email.svg"
+                width={36}
+                height={36}
                 alt="Picture of the author"
               ></Image>
               Email
